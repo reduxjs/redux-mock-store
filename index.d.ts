ac3504b007457aff67ce6f3dc4331743ccbbbe3a
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-03-21 20:10:18 +0000

    Added Typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module '~redux-mock-store/lib' {
+	import * as Redux from 'redux'
+
+	function createMockStore<T>(middlewares?: Redux.IMiddleware<T>[]): createMockStore.mockStore<T>
+
+	namespace createMockStore {
+		type mockStore<T> = (state?: T) => IStore<T>;
+
+		type IStore<T> = {
+			dispatch(action: Redux.IAction | Function): Promise<Redux.IAction>
+			getState(): T
+			getActions(): Object[]
+			clearActions(): void
+			subscribe(): Function
+		}
+	}
+
+	export = createMockStore
+}
+
+declare module 'redux-mock-store/lib' {
+	import main = require('~redux-mock-store/lib')
+	export = main
+}
+
+declare module 'redux-mock-store' {
+	import main = require('~redux-mock-store/lib')
+	export = main
+}
